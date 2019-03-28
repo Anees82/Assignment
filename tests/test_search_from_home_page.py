fe3fcdba53f234d783d8a167946761900f6ae08c
Anees82 <anees_c@hotmail.com>
2019-03-29 04:08:00 +0500

    adding docstring to test cases

--- tests/test_search_from_home_page.py
+++ tests/test_search_from_home_page.py
@@ -12,29 +12,37 @@ from pages.search_page import MediumSearchPage
 class TestMediumSearchUIHomePage(unittest.TestCase):
 
     def setUp(self):
-
         self.driver = webdriver.Chrome()
-
+        self.driver.maximize_window()
         self.home_page = MediumHomePage(self.driver)
         self.search_page = MediumSearchPage(self.driver)
         self.driver.get(self.home_page.url)
 
     def test_user_is_on_home_page(self):
         """
-        VERIFY THAT USER IS REDIRECTED TO THE HOME PAGE SUCCESSFULLY
+        Validate Page Title - RA-001
         """
         self.assertTrue(self.home_page.is_browser_on_page())
 
     def test_search_icon_is_present(self):
+        """
+        Validate search icon - RA-001
+        """
         self.assertIsNotNone(self.driver.find_element_by_css_selector(SEARCH_BAR_ICON))
 
     def test_search_input_is_displayed_on_clicking_search_icon(self):
+        """
+        Click the search icon on the top right of the page - RA-002
+        """
         search_icon = self.driver.find_element_by_css_selector(SEARCH_BAR_ICON)
         search_icon.click()
 
         self.assertIsNotNone(self.home_page.wait_for_css(SEARCH_INPUT))
 
     def test_results_page_url_and_search_results_for_good_search_data(self):
+        """
+        Search from home page with good data - RA-002
+        """
         search_icon = self.driver.find_element_by_css_selector(SEARCH_BAR_ICON)
         search_icon.click()
 
@@ -47,6 +55,9 @@ class TestMediumSearchUIHomePage(unittest.TestCase):
         self.assertTrue(len(self.search_page.wait_for_css(STORIES_BLOCKS)))
 
     def test_search_results_for_bad_search_data(self):
+        """
+        Search from home page with bad data - RA-003
+        """
         search_icon = self.driver.find_element_by_css_selector(SEARCH_BAR_ICON)
         search_icon.click()
 
