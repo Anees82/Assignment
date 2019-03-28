9eda3586a40ab053e6f02614be9614bc674e6b74
Anees82 <49044990+Anees82@users.noreply.github.com>
2019-03-29 03:41:58 +0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,15 @@
+# HOME PAGE #
+BASE_URL = 'https://medium.com'
+GOOD_SEARCH_WORD = 'hello'
+BAD_SEARCH_WORD = 'dhuz'
+
+SEARCH_BAR_ICON = '.svgIcon--search'
+SEARCH_INPUT = '.js-predictiveSearchInput'
+
+SEARCH_RESULTS_DIV = '.js-postList'
+
+# SEARCH PAGE #
+SEARCH_PAGE_URL = f'{BASE_URL}/search'
+
+STORIES_BLOCKS = '.js-block'
+NO_RESULT_BLOCK = '.js-emptyBlock'
---
+++ readme.md
@@ -0,0 +1,14 @@
+### Python
+- this code will work with Python3.
+
+### Pre-Reqs
+- Chrome Driver.
+
+
+### Package installations:
+- run this command from your root directory:
+    > pip install -r requirements.txt
+
+### How to run:
+- run this command from your root directory when you're done with installations:
+    > nosetests -v
